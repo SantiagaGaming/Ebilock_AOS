cd9d5f26cd233e2a6e7747c092e25721bcf50588
SantiagaGaming <ganychev92@gmail.com>
2023-01-18 15:58:08 +0300

    Scripts added

---
+++ Assets/Scripts/AOSObjects/SceneAOSObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using AosSdk.Core.Interaction.Interfaces;
+using AosSdk.Core.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+[AosSdk.Core.Utils.AosObject(name: "AosObject")]
+public class SceneAosObject : AosObjectBase
+{
+    [AosEvent(name: "OnClickObject")]
+    public event AosEventHandlerWithAttribute OnClickObject;
+    public void InvokeOnClick()
+    {
+        OnClickObject?.Invoke(ObjectId);
+    }
+    public void ActionWithObject(string actionName)
+    {
+        OnClickObject?.Invoke(actionName);
+    }
+}
---
+++ Assets/Scripts/InteractableObjects/BaseObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using AosSdk.Core.Interaction.Interfaces;
+using AosSdk.Core.Utils;
+using AosSdk.Core.PlayerModule.Pointer;
+using UnityEngine;
+
+
+public class BaseObject : MonoBehaviour, IClickAble, IHoverAble
+{
+    public bool Button;
+    public bool IsHoverable { get; set; } = true;
+    public bool IsClickable { get; set; } = true;
+
+    [SerializeField] protected SceneAosObject SceneAOSObject;
+
+    protected string helperName;
+
+    public virtual void OnClicked(InteractHand interactHand)
+    {
+        SceneAOSObject.InvokeOnClick();
+    }
+
+    public virtual void OnHoverIn(InteractHand interactHand)
+    {
+    }
+    public virtual void OnHoverOut(InteractHand interactHand)
+    {
+    }
+    public void SetHelperName(string value)
+    {
+        helperName = value;
+    }
+    public string GetAOSName()
+    {
+        if (SceneAOSObject != null)
+            return SceneAOSObject.ObjectId;
+        else return null;
+    }
+    public void EnableObject(bool value)
+    {
+        if (GetComponent<Collider>() != null)
+            GetComponent<Collider>().enabled = value;
+        if (GetComponent<SpriteRenderer>() != null)
+            GetComponent<SpriteRenderer>().enabled = value;
+    }
+}
